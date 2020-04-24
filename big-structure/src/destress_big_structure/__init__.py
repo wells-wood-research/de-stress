0bc191ad01b8f889f3c60ac576b753cfd286b8e1
Chris Wells Wood <cwwoodesq@gmail.com>
2020-04-24 15:05:39 +0100

    Adds simple echo websocket.

--- big-structure/src/destress_big_structure/__init__.py
+++ big-structure/src/destress_big_structure/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_graphql import GraphQLView
+from flask_sockets import Sockets
 
 from .big_structure_models import big_structure_db_session
 from .design_models import designs_db_session
@@ -8,8 +9,23 @@ from .design_models import designs_db_session
 from .schema import schema
 
 app = Flask(__name__)
+sockets = Sockets(app)
 CORS(app)
 app.debug = True
+
+
+@sockets.route("/app-comms")
+def app_comms_socket(ws):
+    """Provides 2-way communication for the app.
+
+    All data will flow through this websocket, to enable live updating
+    of the results. All data is sent as JSON and on the Python side should
+    be mapped to Python dataclasses.
+    """
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
+
 
 app.add_url_rule(
     "/graphql",
