a2f23c508e2fd07cab83d8c59b1e9b54dbddd564
Chris Wells Wood <cwwoodesq@gmail.com>
2020-11-10 12:58:09 +0000

    Feat: Adds basic test and related files for EvoEF2.

---
+++ big-structure/tests/test_evoef.py
@@ -0,0 +1,14 @@
+import typing as t
+
+from destress_big_structure.analysis import run_evoef2
+from destress_big_structure.settings import EVOEF2_BINARY_PATH
+
+
+def test_check_run_evoef_executes():
+    test_path = "tests/testing_files/1aac.pdb"
+
+    with open(test_path) as inf:
+        pdb_string = inf.read()
+
+    run_evoef2(pdb_string=pdb_string, evoef2_binary_path=EVOEF2_BINARY_PATH)
+
