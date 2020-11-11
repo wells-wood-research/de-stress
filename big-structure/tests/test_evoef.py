e60e1e69a686efe8e4aad2d1b5f766d9769164ea
Chris Wells Wood <cwwoodesq@gmail.com>
2020-11-11 09:51:01 +0000

    Fix: Changes hardcoded path to pathlib.Path.

--- big-structure/tests/test_evoef.py
+++ big-structure/tests/test_evoef.py
@@ -1,3 +1,4 @@
+import pathlib
 import typing as t
 
 from destress_big_structure.analysis import run_evoef2
@@ -5,7 +6,7 @@ from destress_big_structure.settings import EVOEF2_BINARY_PATH
 
 
 def test_check_run_evoef_executes():
-    test_path = "tests/testing_files/1aac.pdb"
+    test_path = pathlib.Path("tests/testing_files/1aac.pdb")
 
     with open(test_path) as inf:
         pdb_string = inf.read()
