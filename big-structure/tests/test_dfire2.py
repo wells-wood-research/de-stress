624ce6c1d46044856478e1f20f9f92f80552ef68
MichaelJamesStam <michaelstam@protonmail.com>
2021-02-04 22:37:15 +0000

    Test: Added more tests into test_dfire2.py

--- big-structure/tests/test_dfire2.py
+++ big-structure/tests/test_dfire2.py
@@ -7,8 +7,6 @@ import re
 
 from destress_big_structure.analysis import run_dfire2
 from destress_big_structure.settings import DFIRE2_BINARY_PATH
-
-# from destress_big_structure.elm_types import EvoEF2Output
 from destress_big_structure.schema import Query
 from destress_big_structure.big_structure_models import big_structure_db_session
 
@@ -23,6 +21,39 @@ def test_check_run_dfire2_executes():
         pdb_string=pdb_string, dfire2_binary_path=DFIRE2_BINARY_PATH
     )
 
-    print(dfire2_results)
+    # Testing the results encoding
+    results_json_str = dfire2_results.to_json()
+    assert isinstance(results_json_str, str)
 
-    assert False
+    # Testing that the decoded results have the correct number of fields
+    decoded_results = json.loads(results_json_str)
+    assert len(decoded_results) == 2
+
+    # DFIRE2 results obtained from running the binary file
+    # directly in the command line on the pdb file 1aac.pdb
+    test_dfire2_total = -161.6
+
+    # Comparing the returned DFIRE2 results from the results obtained from directly running
+    # the binary file in the command line (test_dfire2_results)
+    assert dfire2_results.total == test_dfire2_total
+
+    # Querying the data base to get the field names in the
+    # DFIRE2Results table
+    schema = graphene.Schema(query=Query)
+    result = schema.execute(
+        """{  __type(name: "DFIRE2Results") {name fields { name } } }"""
+    )
+
+    # Extracting the results of this query and converting from camel case
+    # to snake case
+    db_column_list = []
+    for i in result.data["__type"]["fields"]:
+        db_column_list.append(re.sub(r"(?<!^)(?=[A-Z])", "_", i["name"]).lower())
+
+    # Removing some extra fields (id, state, state_id)
+    db_column_list = [a for a in db_column_list if a not in ["id", "state", "state_id"]]
+
+    # Checking that the fields from the dfire2_results object
+    # are the same as the fields in the DFIRE2Results table
+    # in the data base
+    assert set(dfire2_results.__dict__.keys()) & set(db_column_list)
