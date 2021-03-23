2561d3cc8a3fb7f81a0a448906206e85eb33eca1
Chris Wells Wood <cwwoodesq@gmail.com>
2021-03-23 22:33:35 +0000

    Refactor: Move all env vars into a single file, shared with compose.

--- big-structure/src/destress_big_structure/settings.py
+++ big-structure/src/destress_big_structure/settings.py
@@ -1,9 +1,6 @@
 import os
 
-import dotenv
-
-dotenv.load_dotenv()
-
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 EVOEF2_BINARY_PATH = os.getenv("EVOEF2_BINARY_PATH")
 DFIRE2_FOLDER_PATH = os.getenv("DFIRE2_FOLDER_PATH")
 ROSETTA_BINARY_PATH = os.getenv("ROSETTA_BINARY_PATH")
