38166e3a47989553d72f3a7a78e4a46be352016c
MichaelJamesStam <michaelstam@protonmail.com>
2022-08-22 22:21:49 +0100

    Refactor: Removing print statement

--- big-structure/src/destress_big_structure/console.py
+++ big-structure/src/destress_big_structure/console.py
@@ -520,7 +520,6 @@ def headless_destress_batch(input_path: str) -> None:
     # Getting a list of all the pdb files in the input path
     pdb_file_list = list(input_path.glob("*.pdb"))
     pdb_file_list = [x for x in pdb_file_list if "ranked" in str(x)]
-    print(pdb_file_list)
 
     # Checking that the list of PDB files is not empty.
     assert pdb_file_list, "There are no PDB files in the input path."
