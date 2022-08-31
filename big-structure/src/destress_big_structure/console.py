e4021da17c44d68ba8dcdd13bf5c1bb90ed45c5d
MichaelJamesStam <michaelstam@protonmail.com>
2022-08-31 16:40:40 +0100

    Fixing error handling

--- big-structure/src/destress_big_structure/console.py
+++ big-structure/src/destress_big_structure/console.py
@@ -466,7 +466,7 @@ def headless_destress(pdb_file: str) -> DesignMetricsOutputRow:
 
         design_metrics_output
 
-    except (KeyError, ValueError):
+    except (KeyError, ValueError, TypeError):
 
         # Setting all the design metrics to None
         design_metrics_output = dict(
